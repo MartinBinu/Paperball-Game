44ba38b9bb2acdc5d9d79053737238845c7514a7
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2020-09-15 11:56:30 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,22 @@
+class Dustbin{
+
+    constructor(x,y,width,height){
+         var option ={
+             'isStatic': true
+         }
+ 
+         this.body = Bodies.rectangle(x,y,width,height,option);
+         this.width = width;
+         this.height = height;
+         World.add(world,this.body);
+    }
+ 
+    display(){
+       push();
+       rectMode(CENTER);
+       rect(this.body.position.x,this.body.position.y,this.width,this.height);
+       fill(255);
+       pop();
+     }
+ 
+ };
---
+++ PaperBall.js
@@ -0,0 +1,24 @@
+class PaperBall{
+
+    constructor(x,y,radius){
+       var options = {
+           'restitution': 0.8,
+           'friction': 0.3,
+           'density': 1.2
+       }
+      
+       this.body = Bodies.circle(x,y,radius,options);
+       this.radius = radius;
+       World.add(world,this.body);
+    }
+ 
+    display(){
+       push(); 
+       translate(this.body.position.x,this.body.position.y);
+       rotate(this.body.angle);
+       ellipseMode(RADIUS);
+       ellipse(0,0,this.radius,this.radius);
+       pop();
+    }
+ 
+ }
